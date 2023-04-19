da1535ab7e48747d2ac04f2174eade07fc5340c6
bruno-vribeiro <vilhenab8@gmail.com>
2023-04-19 00:28:00 -0300

    criando um novo componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Banner from './componente/Banner/Banner'
+import CampoTexto from './componente/Banner/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      
+      <CampoTexto />
     </div>
   );
 }
